ecf5f5697834e16f54efc57db9f0246724822684
Andrew <amatselya@gmail.com>
2017-01-07 00:57:18 +0300

    public int Number8 { get; set; }
    	public int Number8 { get; set; }
    	public int Number8 { get; set; }

--- MyClass.cs
+++ MyClass.cs
@@ -11,4 +11,8 @@ public class MyClass
 	public int Number8 { get; set; }
 
 	public int Number7 { get; set; }
+
+	public int Number8 { get; set; }
+	public int Number8 { get; set; }
+	public int Number8 { get; set; }
 }
