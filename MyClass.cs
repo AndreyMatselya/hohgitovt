3280039d3371f49ecdcb551a99fcbcae36c795e8
Andrew <amatselya@gmail.com>
2017-01-07 00:41:12 +0300

    space

--- MyClass.cs
+++ MyClass.cs
@@ -3,6 +3,7 @@ public class MyClass
 	public int Number1 { get; set; }
 	public int Number2 { get; set; }
 	public int Number3 { get; set; }
+
 	public int Number4 { get; set; }
 	public int Number5 { get; set; }
 	public int Number6 { get; set; }
