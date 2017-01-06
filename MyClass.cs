85af61474abf54a7e8ff0a84ba7cf12440874bc1
Andrew <amatselya@gmail.com>
2017-01-07 00:11:46 +0300

    init

---
+++ MyClass.cs
@@ -0,0 +1,10 @@
+public class MyClass
+{
+	public int Number1 { get; set; }
+	public int Number2 { get; set; }
+	public int Number3 { get; set; }
+	public int Number4 { get; set; }
+	public int Number5 { get; set; }
+	public int Number6 { get; set; }
+	public int Number7 { get; set; }
+}
